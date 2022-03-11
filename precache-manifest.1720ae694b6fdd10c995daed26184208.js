814a3cca791e835b913d5dcff8bf43650828808b
ell-hol <yacine.zahidi@gmail.com>
2022-03-11 11:01:14 +0100

    Updates

---
+++ precache-manifest.1720ae694b6fdd10c995daed26184208.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48bf7406d2f5e0577050",
+    "url": "/zoom-ghpages/static/css/main.44877178.chunk.css"
+  },
+  {
+    "revision": "48bf7406d2f5e0577050",
+    "url": "/zoom-ghpages/static/js/main.48bf7406.chunk.js"
+  },
+  {
+    "revision": "9911f828bf176ce9158b",
+    "url": "/zoom-ghpages/static/js/1.9911f828.chunk.js"
+  },
+  {
+    "revision": "3924abf8fe5614aa0fce",
+    "url": "/zoom-ghpages/static/js/runtime~main.3924abf8.js"
+  },
+  {
+    "revision": "4d931200c63b541d972e059be06adae5",
+    "url": "/zoom-ghpages/index.html"
+  }
+];
